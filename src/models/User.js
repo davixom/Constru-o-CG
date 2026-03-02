721098245c0e08d18ebf8a64a82304b39dccf95a
davixom <davi.torressantos13@gmail.com>
2026-03-02 20:46:43 -0300

    Create User model with schema for User roles and attributes

---
+++ src/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    role: {
+        type: String,
+        enum: ['admin', 'solicitante'],
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+userSchema.pre('save', function(next) {
+    this.updatedAt = Date.now();
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
